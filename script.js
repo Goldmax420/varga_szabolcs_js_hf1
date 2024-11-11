4f027597c49cd0d6ed15a897d14ce5f0c155fdbb
Goldmax420 <137089515+Goldmax420@users.noreply.github.com>
2024-11-11 16:22:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function showMessage() {
+    alert("Hello ez egy üzenet József-től!");
+}
+
+console.log("Varga Szabolcs");
+
+let city = "Magyarország";
+console.log(city);
+
+let country = "Fertőszentmiklós";
+console.log(country);
+
+let x = 9;
+console.log(x);
+console.log(x + 1);
+
+let y = 20;
+console.log(y);
+
+// Az x és az y összeadva
+console.log(x + y);
+// Az x és az y szorzata
+console.log(x * y);
+
+let z = 2641;
+console.log(z);
+
+// A z változó 7-tel való osztási maradéka
+console.log(z % 7);
+
+let favoriteAnimal = prompt("Mi a kedvenc állatod?");
+console.log("A kedvenc állatod: " + favoriteAnimal);
